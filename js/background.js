d8aff5936161afa1966fd5f2d0bd1f8c11b6aec7
Ankush Sharma <49613342+Ansh1414@users.noreply.github.com>
2024-08-22 03:07:43 +0530

    Update notification in background.js

--- js/background.js
+++ js/background.js
@@ -2,7 +2,13 @@
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
 		if (request.newIconPath.length) {
-			alert('ak');
+			 chrome.notifications.create({
+    type: 'basic',
+    iconUrl: 'icon.png',
+    title: 'Hello',
+    message: 'Your background script is running!',
+    priority: 2
+  });
 			// read `newIconPath` from request and read `tab.id` from sender
 		    chrome.browserAction.setIcon({
 		    	path: request.newIconPath,
